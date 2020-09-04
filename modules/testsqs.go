2429fdc19ce6574dc0036a6e9eebde1bcca32d3b
crmohanta <chittaranjan.mohanta@accenture.com>
2020-09-04 12:20:06 +0530

    updated testsqs.go

--- modules/testsqs.go
+++ modules/testsqs.go
@@ -8,7 +8,8 @@ import (
 // SetupSQS create a SQS queue.
 func SetupSQS(ctx *pulumi.Context, name string, delaySeconds int, maxMessageSize int, messageRetentionSeconds int, receiveWaitTimeSeconds int, visibilityTimeoutSeconds int) (*sqs.Queue, error) {
 	//q := CreateRecoResourceName(ctx, name)
-	return sqs.NewQueue(ctx, q, &sqs.QueueArgs{
+	//return sqs.NewQueue(ctx, q, &sqs.QueueArgs{
+	return sqs.NewQueue(ctx{
 		DelaySeconds:             pulumi.Int(delaySeconds),
 		MaxMessageSize:           pulumi.Int(maxMessageSize),
 		MessageRetentionSeconds:  pulumi.Int(messageRetentionSeconds),
